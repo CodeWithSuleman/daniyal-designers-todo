7eb37a59257a8e53e48a1a18b0607a13d67a9523
Abdullah <abdullahsoomro245@gmail.com>
2023-08-06 15:34:08 +0500

    Update

--- lib/views/add_todo_screen.dart
+++ lib/views/add_todo_screen.dart
@@ -1,8 +1,8 @@
-
 import 'package:daniyal_designers_todo/models/todo_model.dart';
 import 'package:daniyal_designers_todo/services/firebase_crud.dart';
 import 'package:daniyal_designers_todo/views/user_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
 class AddTodoScreen extends StatefulWidget {
@@ -13,54 +13,66 @@ class AddTodoScreen extends StatefulWidget {
 }
 
 class _AddTodoScreenState extends State<AddTodoScreen> {
-  List<String> collarNum = ['One', 'Two', 'Three', 'Four'];
+  final List<String> collarNum = ['One', 'Two', 'Three', 'Four'];
   String _currentState = 'One';
   DateTime? selectedDate;
+
   @override
   Widget build(BuildContext context) {
     TextEditingController heightController = TextEditingController();
     TextEditingController waistController = TextEditingController();
     TextEditingController nameController = TextEditingController();
     TextEditingController phoneNumController = TextEditingController();
-    DateTime date = DateTime.now();
-    final double deviceHight = MediaQuery.of(context).size.height;
+    final DateTime date = DateTime.now();
+    final double deviceHeight = MediaQuery.of(context).size.height;
     final double deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
         child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
           child: Column(
             children: [
-              SizedBox(height: deviceHight * 0.09),
+              SizedBox(height: deviceHeight * 0.09),
               Row(
                 children: [
                   const Text(
-                    "Afridi Designers",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+                    "Daniyal Designers",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
+                    ),
                   ),
-                  SizedBox(width: deviceWidth * 0.11),
+                  const Spacer(),
                   Text(
-                    'Date:"${DateFormat('dd-MMMM-yyyy').format(date)}"',
+                    'Date: ${DateFormat('dd-MMMM-yyyy').format(date)}',
                   ),
                 ],
               ),
-              SizedBox(height: deviceHight * 0.02),
+              SizedBox(height: deviceHeight * 0.02),
               Form(
                 child: Column(
                   children: [
                     Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         const Text("Your Height:"),
                         SizedBox(width: deviceWidth * 0.07),
                         TextFormField(
                           controller: heightController,
-                          maxLength: 3,
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly,
+                            LengthLimitingTextInputFormatter(3),
+                          ],
                           keyboardType: TextInputType.number,
+                          textInputAction: TextInputAction.next,
                           decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            isDense: true,
+                            contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 10),
                             constraints:
                                 BoxConstraints(maxWidth: deviceWidth * 0.3),
-                            hintText: "Enter Hight",
+                            hintText: "Enter Height",
                             hintStyle: const TextStyle(fontSize: 12),
                           ),
                         ),
@@ -83,14 +95,14 @@ class _AddTodoScreenState extends State<AddTodoScreen> {
                         ),
                       ],
                     ),
-                    SizedBox(height: deviceHight * 0.02),
+                    SizedBox(height: deviceHeight * 0.02),
                     Row(
                       children: [
                         const Text("Your Collar Number:"),
                         SizedBox(width: deviceWidth * 0.1),
                         SizedBox(
                           width: deviceWidth * 0.3,
-                          height: deviceHight * 0.08,
+                          height: deviceHeight * 0.08,
                           child: DropdownButton<String>(
                             items: collarNum.map((String dropDownStringItem) {
                               return DropdownMenuItem<String>(
@@ -107,7 +119,7 @@ class _AddTodoScreenState extends State<AddTodoScreen> {
                         ),
                       ],
                     ),
-                    SizedBox(height: deviceHight * 0.05),
+                    SizedBox(height: deviceHeight * 0.05),
                     TextFormField(
                       controller: nameController,
                       decoration: InputDecoration(
@@ -116,7 +128,7 @@ class _AddTodoScreenState extends State<AddTodoScreen> {
                             borderRadius: BorderRadius.circular(10),
                           )),
                     ),
-                    SizedBox(height: deviceHight * 0.02),
+                    SizedBox(height: deviceHeight * 0.02),
                     TextFormField(
                       controller: phoneNumController,
                       maxLength: 11,
@@ -136,17 +148,18 @@ class _AddTodoScreenState extends State<AddTodoScreen> {
                             final DateTime? datePicked = await showDatePicker(
                                 context: context,
                                 initialDate: DateTime.now(),
-                                firstDate: DateTime(2022),
+                                firstDate: DateTime.now(),
                                 lastDate: DateTime(2101));
-                            if (datePicked != null ) {
+                            if (datePicked != null) {
                               setState(() {
                                 selectedDate = datePicked;
                               });
                             }
                           },
-                          child: const Text("Enter a date"),
+                          child: Text(selectedDate != null
+                              ? DateFormat('dd-MMMM-yyyy').format(selectedDate!)
+                              : 'Select Date'),
                         ),
-                        Text("${DateTime.parse(DateFormat('dd-MM-yy').format(selectedDate??DateTime.now()))}"),
                       ],
                     )
                   ],
@@ -156,7 +169,8 @@ class _AddTodoScreenState extends State<AddTodoScreen> {
                 onPressed: () {
                   FirebaseCRUD().createUser(
                     User(
-                        selectedDate: DateTime.parse(DateFormat('dd-MM-yy').format(selectedDate??DateTime.now())),
+                        selectedDate: DateTime.parse(DateFormat('dd-MM-yy')
+                            .format(selectedDate ?? DateTime.now())),
                         phoneNum: int.tryParse(phoneNumController.text) ?? 0,
                         name: nameController.text,
                         collarNumber: _currentState,
